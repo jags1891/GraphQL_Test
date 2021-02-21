84b130bb61bda1f7784c81a05e0fae28b48185fe
Srivalli Kasavajhula <Srivalli@Srivallis-MacBook-Pro.local>
2021-02-21 01:25:48 -0800

    graphqlsetup

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import schema from './schema';
+import { graphqlHTTP } from 'express-graphql';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing');
+});
+
+const root = { friend: () => {
+    return {
+        "id": 5658489489,
+        "firstName": "Jagadeesh",
+        "lastName": "Bandlamudi",
+        "gender": "Male",
+        "email": "me@me.com"
+    }
+}};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(8080, ()=> console.log("running on server port localhost:8080/graphql"));
